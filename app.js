047e9a2f07503b092ae71a3dcfe3bb4a1fff0fe7
Santiago <ec2-user@ip-172-31-34-61.us-east-2.compute.internal>
2023-02-18 05:16:14 +0000

    next commit

--- app.js
+++ app.js
@@ -1,66 +1,63 @@
 const express = require('express')
 const axios = require('axios')
+const bp = require('body-parser')
 const TaskManager = require('./task-manager.js')
 
 const app = express()
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
 const port = 3000
 
 var TaskM = new TaskManager();
 
 var numberAsync = 0
 var numberSync = 0
-app.get('/async', async (req, res) => {
+
+app.post('/async', async (req, res) => {
   let string = "Async request "
   let message = string + numberAsync;
-  TaskM.pushAsyncTask({message: message})
-  requests+=1;
-
-  axios.get('https://catfact.ninja/fact')
-    .then(response => {
-        res.send({
-          'statusCode': 200,
-          'body': JSON.stringify({
-                        result: response.data,
-                      })
-        })
-    })
-    .catch(err => {
-        // Handle errors
-        res.send({
-          "statusCode": 502,
-          "body": JSON.stringify({
-            error: err,
-          })
-        })
-        console.error(err);
-    });
+  TaskM.pushAsyncTask({message: req.headers})
+  numberAsync+=1;
+  res.send({
+    'statusCode': 200,
+    'body': JSON.stringify({
+                  result: response.data,
+                })
+  })
 })
 
-app.get('/sync', async (req, res) => {
-  let string = "Sync request "
-  let message = string + numberSync;
-  TaskM.pushSyncTask({message: message})
-  requests+=1;
-
-  axios.get('https://catfact.ninja/fact')
-    .then(response => {
-        res.send({
-          'statusCode': 200,
-          'body': JSON.stringify({
-                        result: response.data,
-                      })
-        })
-    })
-    .catch(err => {
-        // Handle errors
-        res.send({
-          "statusCode": 502,
-          "body": JSON.stringify({
-            error: err,
-          })
-        })
-        console.error(err);
-    });
+app.post('/sync', async (req, res) => {
+  console.log("Sync request received")
+  //console.log(req.body);
+  let task = {
+      headers: req.headers,
+      body: req.body,
+      res: res      
+  } 
+  console.log("body: " + req.body);
+  console.log("Headers from Request: " + task.headers);
+  TaskM.pushSyncTask(task)
+  console.log("Sync request pushed to SyncQueue");
+  // axios.get('https://catfact.ninja/fact')
+  //   .then(response => {
+  //       res.send({
+  //         'statusCode': 200,
+  //         'body': JSON.stringify({
+  //                       result: response.data,
+  //                     })
+  //       })
+  //   })
+  //   .catch(err => {
+  //       // Handle errors
+  //       res.send({
+  //         "statusCode": 502,
+  //         "body": JSON.stringify({
+  //           error: err,
+  //         })
+  //       })
+  //       console.error(err);
+  //   });
 })
 
 app.listen(port, () => {
--- task-manager.js
+++ task-manager.js
@@ -1,34 +1,95 @@
 const TaskQueue = require('./task-queue.js')
+const axios = require('axios');
+let url = '';
 
 class TaskManager {
     constructor() {
         this.asyncQueue = new TaskQueue();
-        this.syncQueue= new TaskQueue();
-        this.rps = 5000;
+        this.syncQueue = new TaskQueue();
+        this.rps = 3000;
         this.init();
     }
 
     pushAsyncTask(request) {
         this.asyncQueue.enqueue(request);
     }
-    pushSyncTask() {
+    pushSyncTask(request) {
         this.syncQueue.enqueue(request);
     }
     init() {
-        setInterval(this.processTask.bind(this),this.rps);
+        setInterval(this.processTask.bind(this), this.rps);
     }
     processTask() {
         if (!this.syncQueue.isEmpty()) {
-            let currTask = this.syncQueue.dequeue();
-            console.log(currTask.message);
+            // let currTask = this.syncQueue.dequeue();
+            // //console.log(currTask);
+            // currTask.res.send({
+            //     "statusCode": 200,
+            //     "message": "Response to Sync Request!"
+            // })
+            let handler = this.handleSync.bind(this);
+            handler();
             return
         } if (!this.asyncQueue.isEmpty()) {
-            currTask = this.ayncQueue.dequeue();
-            console.log(currTask.message);
-            return;
+
         }
         console.log("Both queues are Empty");
     }
+
+    async handleSync() {
+        let currTask = this.syncQueue.dequeue();
+        let inTime = true;
+        const promise1 = postMockResponse();
+        const promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+                resolve( {
+                    "statusCode": 429,
+                    "body": "{}"
+                })
+            }, 5000);
+            reject();
+        })
+        let process = await Promise.race([promise1, promise2]);
+        console.log(process);
+        currTask.res.send(process)
+
+        // axios.get('https://catfact.ninja/fact', {timeout: 25000})
+        //     .then(response => {
+        //         //console.log(currTask.body);
+        //         currTask.res.send(response.data);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //         //Handle errors
+        //         res.send(err);
+        //         console.error(err);
+        //     });
+    }
+    async handleAsync() {
+        let currTask = this.asyncQueue.dequeue();
+        axios.post(
+            url,
+            { "body": currTask.body },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        console.log(currTask.message);
+        return;
+    }
+}
+
+function postMockResponse() {
+    return new Promise((resolve, reject) => {
+        setTimeout(11000);
+        resolve({
+            statusCode: 200,
+            body: "Success response mock"
+        })
+    }) 
+
 }
 
 module.exports = TaskManager
